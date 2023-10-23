ecb43e48ed5544d885142b4d2061e8a48111d915
Brian <bschmidt92065@gmail.com>
2023-10-23 19:04:33 -0400

    added #2 solution

---
+++ #2 - Even Fibonacci Numbers/EvenFibonacciNumbers.py
@@ -0,0 +1,12 @@
+previous = 1
+current = 2
+evenSum = 0
+
+while (current < 4000000):
+    if (current % 2 == 0):
+        evenSum += current
+    temp = current + previous
+    previous = current
+    current = temp
+
+print("Answer: " + str(evenSum))
