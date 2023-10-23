5831845669b9cb5a8e156bd12092aa4ff03d61c3
Brian <bschmidt92065@gmail.com>
2023-10-23 19:09:33 -0400

    added comment header

---
+++ #2 - Even Fibonacci Numbers/EvenFibonacciNumbers.py
@@ -0,0 +1,17 @@
+"""
+Problem #2
+Even Fibonacci Numbers
+"""
+
+previous = 1
+current = 2
+evenSum = 0
+
+while (current < 4000000):
+    if (current % 2 == 0):
+        evenSum += current
+    temp = current + previous
+    previous = current
+    current = temp
+
+print("Answer: " + str(evenSum))
